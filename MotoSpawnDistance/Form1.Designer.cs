4ccecaea907dedb812211fe3d3fd7885cc5cdc29
Marko <MarkoBachynsky1997@gmail.com>
2018-09-03 23:28:05 -0500

    Add project files.

---
+++ MotoSpawnDistance/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿using System.ComponentModel;
+
+namespace MotoSpawnDistance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Desktop;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 73);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(236, 313);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.TabStop = false;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.myDataGridView_SelectionChanged);
+            // 
+            // col1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            this.col1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.col1.HeaderText = "Location";
+            this.col1.Name = "col1";
+            this.col1.ReadOnly = true;
+            // 
+            // col2
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            this.col2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.col2.HeaderText = "Distance";
+            this.col2.Name = "col2";
+            this.col2.ReadOnly = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(71, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textbox_KeyChange);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(153, 39);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textbox_KeyChange);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter your desired X and Y coordinates:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(51, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(133, 42);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 398);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Spawn Distance Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.fix_size);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+    }
+}
+
---
+++ MotoSpawnDistance/Form1.cs
@@ -0,0 +1,190 @@
+﻿using Equin.ApplicationFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MotoSpawnDistance
+{
+    public partial class Form1 : Form
+    {
+        public Location[] spawnLocations = new Location[23];
+        public List<Location> List = new List<Location>(23);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create Spawns
+            spawnLocations[0] = new Location("Balota ", new XY(50, 128));
+            spawnLocations[1] = new Location("Berezino ", new XY(123.5, 61.25));
+            spawnLocations[2] = new Location("Chernogorsk", new XY(68.7, 121.2));
+            spawnLocations[3] = new Location("Devil's Castle", new XY(70.5, 45.5));
+            spawnLocations[4] = new Location("Elektrozavodsk ", new XY(96.5, 132.2));
+            spawnLocations[5] = new Location("Gorka", new XY(91, 70));
+            spawnLocations[6] = new Location("Guglovo", new XY(76, 84));
+            spawnLocations[7] = new Location("Grishino", new XY(64.6, 48));
+            spawnLocations[8] = new Location("Gvozdno", new XY(87.6, 34.2));
+            spawnLocations[9] = new Location("Kabanino", new XY(55,75));
+            spawnLocations[10] = new Location("Kamenka", new XY(16.5, 129));
+            spawnLocations[11] = new Location("Kamyshovo", new XY(113.5, 120.5));
+            spawnLocations[12] = new Location("Komarovo", new XY(39, 123.5));
+            spawnLocations[13] = new Location("Krasnostav", new XY(119.5, 29.8));
+            spawnLocations[14] = new Location("Krutoy Cap", new XY(135.5, 105.5));
+            spawnLocations[15] = new Location("Lopatino", new XY(19.8, 55.8));
+            spawnLocations[16] = new Location("Petrovka", new XY(44.5, 29.7));
+            spawnLocations[17] = new Location("Pogorevka", new XY(48.8, 82.6));
+            spawnLocations[18] = new Location("Prigorodki", new XY(80, 120));
+            spawnLocations[19] = new Location("Pusta", new XY(93.2, 112.9));
+            spawnLocations[20] = new Location("Solnichniy", new XY(129, 86));
+            spawnLocations[21] = new Location("Vybor", new XY(29, 65));
+            spawnLocations[21] = new Location("Vyshnoye", new XY(61, 86));
+            spawnLocations[22] = new Location("Zelenogorsk", new XY(23, 94));
+            orderList();
+            fillTable();
+        }
+
+        private static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)) * 100;
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void orderList()
+        {
+            List.Clear();
+            for (int i = 0; i < spawnLocations.Length; i++)
+            {
+                if (List.Count == 0) List.Add(spawnLocations[0]);
+                else
+                {
+                    for (int j = 0; j < List.Count; j++)
+                    {
+                        Location previousLocation = List.ElementAt(j);
+                        Location currentLocation = spawnLocations[i];
+                        previousLocation.distance = GetDistance(Double.Parse(textBox1.Text) + .5, Double.Parse(textBox2.Text) + .5, previousLocation.coordinates.X, previousLocation.coordinates.Y);
+                        currentLocation.distance = GetDistance(Double.Parse(textBox1.Text), Double.Parse(textBox2.Text), currentLocation.coordinates.X, currentLocation.coordinates.Y);
+                        if (previousLocation.distance >= currentLocation.distance) // if new location is farther than previous location, add and break loop
+                        {
+                            List.Insert(j, currentLocation);
+                            break;
+                        }
+                        if (j == List.Count - 1) List.Add(currentLocation);
+                        Debug.WriteLine("PL = " + previousLocation.name + ", " + Math.Round(previousLocation.distance) + " - CL = " + currentLocation.name + ", " + Math.Round(currentLocation.distance));
+
+                        }
+                    }
+            }
+            foreach(Location l in List)
+            {
+                Debug.WriteLine(l.name + " - " + Math.Round(l.distance));
+            }
+
+        }
+
+        private void fillTable()
+        {
+            dataGridView1.Rows.Clear();
+                foreach (Location location in List)
+                {
+                    dataGridView1.Rows.Add(location.name, Math.Round(location.distance).ToString("#,#", CultureInfo.InvariantCulture) + " meters");
+                }
+
+            for (int i = 0; i < dataGridView1.Columns.Count - 1; i++)
+            {
+                dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
+            dataGridView1.Columns[dataGridView1.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                int colw = dataGridView1.Columns[i].Width;
+                dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                dataGridView1.Columns[i].Width = colw;
+            }
+        }
+
+        private void textbox_KeyChange(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.All(char.IsDigit) || !textBox2.Text.All(char.IsDigit)) textBox1.Text = "0";
+            if (textBox1.Text.Equals("") || textBox2.Text.Equals("")) return;
+            orderList();
+            fillTable();
+        }
+
+        private void myDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+        }
+
+        private void fix_size(object sender, EventArgs e)
+        {
+            Width = 300;
+            Height = 437;
+        }
+    }
+}
+
+public class XY
+{
+    public double X;
+    public double Y;
+
+    public XY()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    public XY(int a, int b)
+    {
+        X = a;
+        Y = b;
+    }
+
+    public XY(double a, double b)
+    {
+        X = a;
+        Y = b;
+    }
+
+    override public string ToString()
+    {
+        return X + ", " + Y;
+    }
+
+}
+
+
+public class Location
+{
+    public string name;
+    public XY coordinates;
+    public double distance;
+
+    public Location()
+    {
+        name = "blank";
+        coordinates = new XY(0, 0);
+    }
+
+    public Location(string n, XY xy)
+    {
+        name = n;
+        coordinates = xy;
+    }
+
+}
